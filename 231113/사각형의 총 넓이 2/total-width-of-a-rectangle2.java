029b8cb23ad01090a60a0d05b2ccfb35d59c9f80
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-13 11:15:34 +0900

    Title: 사각형의 총 넓이 2, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 231113/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 231113/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -10,10 +10,10 @@ public class Main {
         for (int i = 0; i < cnt; i++) {
             String str = br.readLine();
             String[] split = str.split(" ");
-            x1 = Integer.parseInt(split[0]);
-            y1 = Integer.parseInt(split[1]);
-            x2 = Integer.parseInt(split[2]);
-            y2 = Integer.parseInt(split[3]);
+            x1 = Integer.parseInt(split[0]) + OFF_SET;
+            y1 = Integer.parseInt(split[1]) + OFF_SET;
+            x2 = Integer.parseInt(split[2]) + OFF_SET;
+            y2 = Integer.parseInt(split[3]) + OFF_SET;
             for (int j = x1; j < x2; j++) {
                 for (int k = y1; k <y2; k++) {
                     grid[j][k]++;
