03991e2caad1ddef9f882d43ac05634910a3412b
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-13 10:57:43 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231113/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,41 @@
+import java.io.*;
+public class Main {
+    static int OFFSET_TEMP = 20;
+    static int MAX_N_TEMP = OFFSET_TEMP * 2 + 1;
+    static char[] sectionArr = new char[MAX_N_TEMP];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 회색 타일이 존재함
+        // L : 흰색 R : 검은색
+        // 타임 포함 총 x칸의 타일을 뒤집음
+        // 흰색, 검은색 타일 수를 각각 출력하기
+        int cnt = Integer.parseInt(br.readLine());
+        int curIdx = OFFSET_TEMP;
+        for (int i = 0; i < cnt; i++) {
+            String str = br.readLine();
+            String[] split = str.split(" ");
+            int x = Integer.parseInt(split[0]);
+            char direction = split[1].charAt(0);
+
+            if(direction == 'R'){
+                while(x-- > 0){
+                    sectionArr[curIdx] = direction;
+                    if(x > 0) curIdx++;
+                }
+            }
+            else{
+                while (x-- > 0){
+                    sectionArr[curIdx] = direction;
+                    if(x > 0) curIdx--;
+                }
+            }
+        }
+        int whiteSection = 0;
+        int blackSection = 0;
+        for (int i = 0; i < MAX_N_TEMP; i++) {
+            if(sectionArr[i] == 'R') blackSection++;
+            else if (sectionArr[i] == 'L') whiteSection++;
+        }
+        System.out.println(whiteSection + " " +  blackSection);
+    }
+}
