370cf20a1e70979e995d2f4fd1f6d2b19afd1fd1
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-14 22:04:09 +0900

    Title: 연속되는 수 4, Time: 110ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231114/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 연속된 부분 수열 뽑기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        int[] arr = new int[cnt];
+        // 입력값 넣기
+        for (int i = 0; i < cnt; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        int n = 1, ans = 1;
+        for (int i = 1; i < cnt; i++) {
+            if(arr[i-1] < arr[i]) n++;
+            else n = 1;
+            ans = Math.max(ans, n);
+        }
+        System.out.println(ans);
+    }
+}
