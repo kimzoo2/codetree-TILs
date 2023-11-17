776650ec1b05b1dd22ec96f218273e52faae3582
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-17 21:20:12 +0900

    Title: 벌금은 누구에게, Time: 125ms, Memory: 9MB, Status: Passed - CodeTree

--- 231117/벌금은 누구에게/who-will-pay.java
+++ 231117/벌금은 누구에게/who-will-pay.java
@@ -1,27 +1,22 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] split = br.readLine().split(" ");
         int n = Integer.parseInt(split[0]);
         int m = Integer.parseInt(split[1]);
         int k = Integer.parseInt(split[2]);
         int[] students = new int[n+1];
-        int[] query = new int[m];
-        for (int i = 0; i < m; i++) {
-            query[i] = Integer.parseInt(br.readLine());
-        }
         int idx = -1;
         for (int i = 0; i < m; i++) {
-            int num = query[i];
+            int num = Integer.parseInt(br.readLine());
             students[num]++;
-            if(students[num] == k){
+            if(students[num] >= k){
                 idx = num;
                 break;
             }
         }
-
         System.out.println(idx);
     }
 }
