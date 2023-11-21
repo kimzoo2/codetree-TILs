83f3e6a07b7de7c40267c1bda74f6848e83f97a3
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-21 21:48:45 +0900

    Title: 되돌아오기, Time: 101ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231121/되돌아오기/come-back.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class Main {
+    static int OFFSET = 1_000;
+    static int MAX_LEN = OFFSET * 2 + 1;
+    static int SECOND = 0;
+    static int X = OFFSET;
+    static int Y = OFFSET;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        int dirNum = 0;
+        int[][] grid = new int[MAX_LEN][MAX_LEN];
+        int x = OFFSET;
+        int y = OFFSET;
+
+        for (int i = 0; i < cnt; i++) {
+            String[] strings = br.readLine().split(" ");
+            char direction = strings[0].charAt(0);
+            int distance = Integer.parseInt(strings[1]);
+            dirNum = getDirNum(direction);
+            if(getSecond(distance,dirNum)) break;
+        }
+
+        System.out.println(SECOND == 0 ? -1 : SECOND);
+    }
+
+    private static boolean getSecond(int distance, int dirNum) {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        for (int j = 0; j < distance; j++) {
+            SECOND++;
+            X += dx[dirNum];
+            Y += dy[dirNum];
+
+            if(X == OFFSET && Y == OFFSET){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int getDirNum(char ch){
+        char[] chArr = {'E', 'S', 'W', 'N'};
+        for (int i = 0; i < 4; i++) {
+            if(ch== chArr[i]) return i;
+        }
+        return 0;
+    }
+}
