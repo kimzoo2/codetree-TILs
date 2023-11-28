7c406c45f0169ab9380cb3584b16646d3bbed789
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-28 11:47:56 +0900

    Title: 체크판위에서 2, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231128/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+
+        char[][] grid = new char[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = str.charAt(j);
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                for (int k = i+1; k < n-1; k++) {
+                    for (int l = j+1; l < m-1; l++) {
+                            if(grid[0][0] != grid[i][j] &&
+                            grid[i][j] != grid[k][l] &&
+                            grid[k][l] != grid[n-1][m-1]) cnt++;
+                    }
+               }
+           }
+        }
+        System.out.println(cnt);
+    }
+}
