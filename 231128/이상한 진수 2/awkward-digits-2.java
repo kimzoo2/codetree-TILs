020b194ac1991cae96eb6d60deb8a03bdc1b2733
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-28 15:02:43 +0900

    Title: 이상한 진수 2, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

--- 231128/이상한 진수 2/awkward-digits-2.java
+++ 231128/이상한 진수 2/awkward-digits-2.java
@@ -9,16 +9,7 @@ public class Main {
         for (int i = 0; i < len; i++) {
             String tempStr = str;
             char ch = str.charAt(i);
-            if(ch == '0'){
-                ch = '1';
-            }else{
-                ch = '0';
-            }
-            if(i > 0) {
-                tempStr = str.substring(0, i) + ch + str.substring(i+1);
-            }else{
-                tempStr = ch + str.substring(i+1);
-            }
+            tempStr = str.substring(0, i) + (char)((int)'0' + '1' - ch) + str.substring(i+1);
             max = Math.max(max, Integer.valueOf(tempStr, 2));
         }
 
