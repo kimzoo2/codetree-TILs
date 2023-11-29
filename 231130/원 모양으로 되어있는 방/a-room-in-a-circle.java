fbba9b2cd0b081ff411adaedd8f72cbcfec28ffe
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-11-30 00:55:44 +0900

    Title: 원 모양으로 되어있는 방, Time: 113ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231130/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] room = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            room[i] = Integer.parseInt(br.readLine());
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                int distance = 0;
+                if (i == j) continue;
+                else if (i > j) distance = Math.abs(n - i) + j;
+                else distance = Math.abs(j - i);
+                sum += room[j] * distance;
+            }
+            min = Math.min(min, sum);
+        }
+
+        System.out.println(min);
+    }
+}
