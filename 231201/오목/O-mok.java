e3e65aa136585008e8a31d55dc5a53ad340e7b1f
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-12-01 15:37:02 +0900

    Title: 오목, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231201/오목/O-mok.java
@@ -0,0 +1,98 @@
+import java.io.*;
+public class Main {
+    static int LEN = 19;
+    static int[][] board = new int[LEN+1][LEN+1];
+    static int ansX = 0;
+    static int ansY = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 1; i < LEN; i++) {
+            String[] split = br.readLine().split(" ");
+            for (int j = 1; j < LEN; j++) {
+                board[i][j] = Integer.parseInt(split[j-1]);
+            }
+        }
+        int color = 0;
+        for (int i = 1; i < LEN; i++) {
+            for (int j = 1; j < LEN; j++) {
+                if (board[i][j] != 0) {
+                    color = isCorrect(i, j);
+                    break;
+                }
+            }
+            if(color > 0) break;
+        }
+        System.out.println(color);
+        if(color > 0) {
+            System.out.println(ansX + " " + ansY);
+        };
+    }
+
+    private static int isCorrect(int x, int y) {
+        int color = 0;
+        // 가로 확인
+        if(containsRow(x, y) || containsColumn(x, y) ||
+           containsUpwardRight(x, y) || containsDownwardRight(x, y))
+        {
+            color =board[x][y];
+        };
+
+        return color;
+    }
+
+    private static boolean containsRow(int x, int y){
+        int color = board[x][y];
+        for (int i = y; i < y + 5; i++) {
+            // 범위를 넘기거나 다른 색이면 false
+            if (!inRange(x, i) || board[x][i] != color) {
+                return false;
+            }
+        }
+        ansX = x;
+        ansY = y+2;
+        return true;
+    }
+    private static boolean containsColumn(int x, int y){
+        int color = board[x][y];
+        for (int i = x; i < y + 5; i++) {
+            // 범위를 넘기거나 다른 색이면 false
+            if (!inRange(x, i) || board[i][y] != color) {
+                return false;
+            }
+        }
+        ansX = x+2;
+        ansY = y;
+        return true;
+    }
+
+    private static boolean containsUpwardRight(int x, int y){
+        int color = board[x][y];
+        for (int i = x; i < x + 5; i++) {
+            for (int j = x; j < x + 1; j++) {
+                if (!inRange(x, i) || board[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+        ansX = x+2;
+        ansY = y+2;
+        return true;
+    }
+
+    private static boolean containsDownwardRight(int x, int y){
+        int color = board[x][y];
+        for (int i = x; i > x - 5; i--) {
+            if (!inRange(i, y) || board[i][y++] != color) {
+                return false;
+            }
+        }
+        ansX = x-2;
+        ansY = y+2;
+        return true;
+    }
+
+    private static boolean inRange(int x, int y) {
+        return (x >= 0 && y >= 0 && x < LEN && y < LEN);
+    }
+}
