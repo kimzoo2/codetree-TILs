f94328004e4cbe99577892ea764fc449fadeb214
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-12-01 15:53:58 +0900

    Title: 오목, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231201/오목/O-mok.java
+++ 231201/오목/O-mok.java
@@ -2,6 +2,7 @@ import java.io.*;
 public class Main {
     static int LEN = 19;
     static int[][] board = new int[LEN+1][LEN+1];
+    static int color = 0;
     static int ansX = 0;
     static int ansY = 0;
     public static void main(String[] args) throws IOException {
@@ -10,38 +11,35 @@ public class Main {
         for (int i = 1; i < LEN; i++) {
             String[] split = br.readLine().split(" ");
             for (int j = 1; j < LEN; j++) {
-                board[i][j] = Integer.parseInt(split[j-1]);
+                board[i][j] = Integer.parseInt(split[j - 1]);
             }
         }
-        int color = 0;
         for (int i = 1; i < LEN; i++) {
             for (int j = 1; j < LEN; j++) {
-                if (board[i][j] != 0) {
-                    color = isCorrect(i, j);
+                if (board[i][j] != 0 && isCorrect(i, j)) {
                     break;
                 }
             }
-            if(color > 0) break;
+            if (color > 0) break;
         }
         System.out.println(color);
-        if(color > 0) {
+        if (color > 0) {
             System.out.println(ansX + " " + ansY);
-        };
+        }
+        ;
     }
 
-    private static int isCorrect(int x, int y) {
-        int color = 0;
+    private static boolean isCorrect(int x, int y) {
         // 가로 확인
-        if(containsRow(x, y) || containsColumn(x, y) ||
-           containsUpwardRight(x, y) || containsDownwardRight(x, y))
-        {
-            color =board[x][y];
-        };
-
-        return color;
+        if (containsRow(x, y) || containsColumn(x, y) ||
+                containsUpwardRight(x, y) || containsDownwardRight(x, y)) {
+            color = board[x][y];
+            return true;
+        }
+        return false;
     }
 
-    private static boolean containsRow(int x, int y){
+    private static boolean containsRow(int x, int y) {
         int color = board[x][y];
         for (int i = y; i < y + 5; i++) {
             // 범위를 넘기거나 다른 색이면 false
@@ -50,9 +48,10 @@ public class Main {
             }
         }
         ansX = x;
-        ansY = y+2;
+        ansY = y + 2;
         return true;
     }
+
     private static boolean containsColumn(int x, int y) {
         int color = board[x][y];
         for (int i = x; i < x + 5; i++) {
