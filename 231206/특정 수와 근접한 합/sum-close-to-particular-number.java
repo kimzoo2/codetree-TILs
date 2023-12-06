e29f7839fa728a89c827f82209e0b9acc071eefd
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-12-06 21:26:42 +0900

    Title: 특정 수와 근접한 합, Time: 189ms, Memory: 8MB, Status: Passed - CodeTree

--- 231206/특정 수와 근접한 합/sum-close-to-particular-number.java
+++ 231206/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -21,6 +21,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                if(i == j) continue;
                 int sum = arrSum - arr[i] - arr[j];
                 min = Math.min(min, Math.abs(sum-k));
             }
