d8855be964e6a2087af6ba3e56d6b32eaeb3bc17
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-12-08 18:21:13 +0900

    Title: 특정 구간의 원소 평균값, Time: 106ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231208/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,33 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n-i; j++) {
+                int sum = 0;
+                for (int k = j; k < j+i; k++) {
+                    sum+= arr[k];
+                }
+                float avg = (float)sum/i;
+
+                // 평균값이 구간의 값 중 하나가 되면 카운트를 증가한다.
+                for (int k = j; k < j+i; k++) {
+                    if(avg == arr[k]){
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
