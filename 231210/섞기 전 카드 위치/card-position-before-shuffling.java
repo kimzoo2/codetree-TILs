3bae3cd8fbfde96b0a40f4c3715965554c615fdf
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-12-10 23:33:03 +0900

    Title: 섞기 전 카드 위치, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 231210/섞기 전 카드 위치/card-position-before-shuffling.java
+++ 231210/섞기 전 카드 위치/card-position-before-shuffling.java
@@ -8,13 +8,27 @@ public class Main {
         String[] a = br.readLine().split(" ");
         String[] b = br.readLine().split(" ");
 
+         int[] temp = new int[n];
         for (int i = 0; i < n; i++) {
-            int idx = Integer.parseInt(a[i])-1; // 2
-            bArr[idx] = Integer.parseInt(b[i]); // 2 = 102;
+            aArr[i] = Integer.parseInt(a[i])-1;
+            bArr[i] = Integer.parseInt(b[i]);
+            temp[i] = bArr[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(bArr[i]);
+        // 1    2    4   3  0
+        // 102 103 105 104 101
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = aArr[j]; // 1 2 4 3 0
+                temp[j] = bArr[idx]; // 0 = 103, 1= 105, 2= 101, 3= 104, 4=102
+            }
+            for (int j = 0; j < n; j++) {
+                bArr[j] = temp[j];
+            }
+        }
+
+        for (int i : temp) {
+            System.out.println(i);
         }
     }
 }
