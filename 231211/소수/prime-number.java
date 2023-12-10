305dcf2c999e6e55410b80e7282fd5cd71c737f8
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2023-12-11 00:19:35 +0900

    Title: 소수, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231211/소수/prime-number.java
@@ -0,0 +1,31 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int sum = 0;
+        int min = b;
+        for (int i = a; i < b; i++) {
+            if(isPrime(i)){
+                min = Math.min(min, i);
+                sum += i;
+            }
+        }
+        if(sum == 0){
+            System.out.println(-1);
+        }else{
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    private static boolean isPrime(int n){
+        if(n == 1) return false;
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+}
