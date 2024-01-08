eee54a89751ccbf729b8e2cc3065d94e3aef418d
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-01-08 23:27:06 +0900

    Title: 삼각형 만들기, Time: 137ms, Memory: 9MB, Status: Passed - CodeTree

--- 240108/삼각형 만들기/create-triangle.java
+++ 240108/삼각형 만들기/create-triangle.java
@@ -34,10 +34,8 @@ public class Main {
         if(x1 == x2 || x2 == x3 || x1 == x3){
             // y가 평행한가?
             if(y1 == y2 || y2 == y3 ||y1 == y3){
-                //
-                int a = (x1 * y2 + x2 * y3 + x3 * y1);
-                int b = (x2 * y1 + x3 * y2 + x1 * y3);
-                area = (a-b);
+                area = Math.abs(((x1 * y2 + x2 * y3 + x3 * y1) -
+                        (x2 * y1 + x3 * y2 + x1 * y3)));
             }
         }
         return area;
