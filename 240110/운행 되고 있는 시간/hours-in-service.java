03f1b8be46cbe2adf1da702ef8f7c5f04f952787
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-01-10 23:54:07 +0900

    Title: 운행 되고 있는 시간, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

--- 240110/운행 되고 있는 시간/hours-in-service.java
+++ 240110/운행 되고 있는 시간/hours-in-service.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class Main {
-    static int MAX_LEN = 10;
+    static int MAX_LEN = 1000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int cnt = Integer.parseInt(br.readLine());
