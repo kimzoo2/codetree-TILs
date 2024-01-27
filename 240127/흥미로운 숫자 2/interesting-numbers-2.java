82835e9e9f3189a165c1a5f607345e1b9c6a9da3
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-01-27 17:30:59 +0900

    Title: 흥미로운 숫자 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240127/흥미로운 숫자 2/interesting-numbers-2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int x = Integer.parseInt(input[0]);
+		int y = Integer.parseInt(input[1]);
+
+		int ans = 0;
+		for (int i = x; i <= y; i++) {
+			int num = i;
+			int len = String.valueOf(num).length();
+			Map<Integer, Integer> map = new HashMap<>();
+
+			for (int j = 0; j < len; j++) {
+				int key = num % 10;
+				map.put(key, map.getOrDefault(key, 0)+1);
+				num /= 10;
+			}
+			int size = map.size();
+			if(size != 2) continue;
+
+			for (Integer value : map.keySet()) {
+				if(value == 1) ans++;
+			}
+		}
+
+		System.out.println(ans);
+    }
+}
