7766ac4d8f16a9dbb740ef7617610bc064e5f6ca
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-02-04 16:43:01 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 104ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240204/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		//크기가 다른 정수 A, B, C가 주어집니다.
+		// A와 B를 여러 번 사용할 수 있고 사용한 숫자들의 합이 C보다 작거나 같으면서 최대가 되도록 하는 프로그램을 작성해보세요.
+
+		// 17 25 77
+		// => 76
+
+		// A+B+A <= 77
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] split = br.readLine().split(" ");
+		int a = Integer.parseInt(split[0]);
+		int b = Integer.parseInt(split[1]);
+		int c = Integer.parseInt(split[2]);
+
+		int ans = 0;
+		for (int i = 0; i < c; i++) {
+			int sum = a + b;
+			sum = getSum(a + b, a, c);
+
+			ans = Math.max(ans, sum);
+
+			sum = getSum(a + b, b, c);
+
+			ans = Math.max(ans, sum);
+
+			sum = getSum(a + b, a + b, c);
+
+			ans = Math.max(ans, sum);
+		}
+
+		System.out.println(ans);
+	}
+
+	private static int getSum(int sum, int a, int c) {
+		while (sum + a <= c) {
+			sum += a;
+		}
+		return sum;
+	}
+}
