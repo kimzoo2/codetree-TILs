c66a2cd6f78cb899726b3472c5bccd071b286b35
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-02-15 10:24:09 +0900

    Title: 독서실의 거리두기 5, Time: 115ms, Memory: 8MB, Status: Passed - CodeTree

--- 240215/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 240215/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -7,28 +7,53 @@ public class Main {
 		int cnt = Integer.parseInt(br.readLine());
 		String seatingChart = br.readLine();
 
-		int max = Integer.MIN_VALUE;
-		// 좌석 위치
+		int oldSeat = Integer.MAX_VALUE;
+
+		// 기존 거리 구하기
 		for (int i = 0; i < cnt; i++) {
-			// 이미 앉은 좌석
-			if(seatingChart.charAt(i) > '0') continue;
+			// 앉은 자리가 0이면
+			if (seatingChart.charAt(i) == '0')
+				continue;
 			int minDistance = Integer.MAX_VALUE;
 
 			// 앞으로 인접한 대상 있는지 확인한다.
 			for (int j = i; j >= 0; j--) {
-				if(seatingChart.charAt(j) > '0'){
-					minDistance = min(minDistance, abs(i-j));
+				if (seatingChart.charAt(j) > '0' && i!=j) {
+					minDistance = min(minDistance, abs(i - j));
 					break;
 				}
 			}
 			for (int j = i; j < cnt; j++) {
-				if(seatingChart.charAt(j) > '0'){
-					minDistance = min(minDistance, abs(i-j));
+				if (seatingChart.charAt(j) > '0' && i!=j) {
+					minDistance = min(minDistance, abs(i - j));
 					break;
 				}
 			}
-			max = max(max, minDistance);
+			oldSeat = Math.min(oldSeat, minDistance);
 		}
-		System.out.println(max);
+		int newSeat = Integer.MIN_VALUE;
+		// 좌석 거리 구하기
+		for (int i = 0; i < cnt; i++) {
+			if (seatingChart.charAt(i) > '0') // 이미 앉은 자리는 앉지 않는다.
+				continue;
+			int minDistance = Integer.MAX_VALUE;
+
+			// 앞으로 인접한 대상 있는지 확인한다.
+			for (int j = i; j >= 0; j--) {
+				if (seatingChart.charAt(j) > '0') {
+					minDistance = min(minDistance, abs(i - j));
+					break;
+				}
+			}
+			for (int j = i; j < cnt; j++) {
+				if (seatingChart.charAt(j) > '0') {
+					minDistance = min(minDistance, abs(i - j));
+					break;
+				}
+			}
+			newSeat = Math.max(newSeat, minDistance);
+		}
+		
+		System.out.println(Math.min(newSeat, oldSeat));
 	}
 }
