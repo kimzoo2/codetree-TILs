948074282bc6ddda59f8a32d7fc523d0161a0219
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-02-15 10:32:42 +0900

    Title: 독서실의 거리두기 5, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

--- 240215/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 240215/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -6,54 +6,28 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int cnt = Integer.parseInt(br.readLine());
 		String seatingChart = br.readLine();
+		char[] charArr = seatingChart.toCharArray();
+		int max = Integer.MIN_VALUE;
 
-		int oldSeat = Integer.MAX_VALUE;
-
-		// 기존 거리 구하기
 		for (int i = 0; i < cnt; i++) {
-			// 앉은 자리가 0이면
-			if (seatingChart.charAt(i) == '0')
-				continue;
-			int minDistance = Integer.MAX_VALUE;
-
-			// 앞으로 인접한 대상 있는지 확인한다.
-			for (int j = i; j >= 0; j--) {
-				if (seatingChart.charAt(j) > '0' && i!=j) {
-					minDistance = min(minDistance, abs(i - j));
-					break;
-				}
+			if(charArr[i] == '0'){
+				charArr[i] = '1';
+				int distance = getDistance(charArr, cnt);
+				max = Math.max(max, distance);
+				charArr[i] = '0';
 			}
-			for (int j = i; j < cnt; j++) {
-				if (seatingChart.charAt(j) > '0' && i!=j) {
-					minDistance = min(minDistance, abs(i - j));
-					break;
-				}
-			}
-			oldSeat = Math.min(oldSeat, minDistance);
 		}
-		int newSeat = Integer.MIN_VALUE;
-		// 좌석 거리 구하기
-		for (int i = 0; i < cnt; i++) {
-			if (seatingChart.charAt(i) > '0') // 이미 앉은 자리는 앉지 않는다.
-				continue;
-			int minDistance = Integer.MAX_VALUE;
 
-			// 앞으로 인접한 대상 있는지 확인한다.
-			for (int j = i; j >= 0; j--) {
-				if (seatingChart.charAt(j) > '0') {
-					minDistance = min(minDistance, abs(i - j));
-					break;
-				}
+		System.out.println(max);
+	}
+
+    private static int getDistance(char[] seatArr, int len) {
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < len; i++) {
+			for (int j = i+1; j < len; j++) {
+				if(seatArr[i] > '0' && seatArr[j] > '0') min = Math.min(min, Math.abs(i-j));
 			}
-			for (int j = i; j < cnt; j++) {
-				if (seatingChart.charAt(j) > '0') {
-					minDistance = min(minDistance, abs(i - j));
-					break;
-				}
-			}
-			newSeat = Math.max(newSeat, minDistance);
 		}
-		
-		System.out.println(Math.min(newSeat, oldSeat));
+		return min;
 	}
 }
