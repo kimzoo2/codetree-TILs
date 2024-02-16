c4245bce80227d7a1cc2597a6d04c7dc49a18b45
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-02-16 14:41:54 +0900

    Title: 숫자들의 최대 차, Time: 133ms, Memory: 9MB, Status: Passed - CodeTree

--- 240216/숫자들의 최대 차/maximum-difference-in-numbers.java
+++ 240216/숫자들의 최대 차/maximum-difference-in-numbers.java
@@ -20,12 +20,9 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-		Arrays.sort(arr);
-		int min = arr[0];
-		int max = arr[n-1];
 
 		int ans = Integer.MIN_VALUE;
-		for (int i = min; i < max; i++) {
+		for (int i = 1; i < 10_000; i++) {
 			int cnt = 0;
 			for (int j = 0; j < n; j++) {
 				if(i<=arr[j] && arr[j]<=i+k) cnt++;
