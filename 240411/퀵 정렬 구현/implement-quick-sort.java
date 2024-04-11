40fe96e5b416380ad3a212b4a1e8e560c3b97d3d
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-04-11 12:08:48 +0900

    Title: 퀵 정렬 구현, Time: 808ms, Memory: 28MB, Status: Passed - CodeTree

--- 240411/퀵 정렬 구현/implement-quick-sort.java
+++ 240411/퀵 정렬 구현/implement-quick-sort.java
@@ -1,71 +1,46 @@
 import java.io.*;
 public class Main {
-    static int N;
+    public static final int MAX_N = 100000;
+	public static int n;
+	public static int[] arr = new int[MAX_N];
     public static void main(String[] args) throws IOException {
 		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		n = Integer.parseInt(br.readLine());
 		String[] split = br.readLine().split(" ");
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(split[i]);
 		}
-		quickSort(arr, 0, N-1);
+		quickSort(0, n - 1);
 
-		for (int i : arr) {
-			System.out.print(i + " ");
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
 		}
 	}
+    public static int partition(int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1;
 
-	public static int partition(int[] arr, int low, int high){
-		int i = low -1; // blue
-		int pivot = getPivot(arr, low, high);
-
-		for (int j = low; j <= high; j++) {
-			if(arr[j] < pivot){
+		for(int j = low; j < high; j++)
+			if(arr[j] < pivot) {
 				i++;
-				if(arr[j] < arr[i]){
-					swapValue(arr, i, j);
-				}
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
 			}
-		}
-		if(arr[i + 1] > arr[high]) {
-			swapValue(arr, i + 1, high);
-		}
+
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+
 		return i + 1;
 	}
 
-	private static int getPivot(int[] arr, int low, int high) {
-		if(high - low <= 3){
-			return high;
-		}
-		int pivot = getMid(arr, low, high);
-		swapValue(arr, pivot, high);
-		return arr[high];
-	}
+	public static void quickSort(int low, int high) {
+		if(low < high) {
+			int pos = partition(low, high);
 
-	private static void swapValue(int[] arr, int i, int j){
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
-
-	private static int getMid(int[] arr, int low, int high){
-		int mid = N/2;
-		if(arr[low] > arr[mid]){
-			if(arr[low] < arr[high]) return low;
-		}else if(arr[mid] > arr[low]){
-			if(arr[mid] < arr[high]) return mid;
-		}
-		return high;
-	}
-
-	private static void quickSort(int[] arr, int low, int high){
-		int pos;
-		if(low < high){
-			pos = partition(arr, low, high);
-
-			quickSort(arr, low ,pos-1);
-			quickSort(arr, pos + 1, high);
+			quickSort(low, pos - 1);
+			quickSort(pos + 1, high);
 		}
 	}
 }
