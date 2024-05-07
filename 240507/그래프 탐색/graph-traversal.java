d6e607e6483dd56c439d3573003712a73f3af00a
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-05-07 12:35:05 +0900

    Title: 그래프 탐색, Time: 212ms, Memory: 15MB, Status: Passed - CodeTree

--- 240507/그래프 탐색/graph-traversal.java
+++ 240507/그래프 탐색/graph-traversal.java
@@ -1,32 +1,36 @@
 import java.io.*;
+import java.util.ArrayList;
 public class Main {
     static int CNT = 0;
-	static int[][] GRAPH = new int[1001][1001];
 	static boolean[] checked = new boolean[1001];
-    private static void searchGraph(int vertex){
-		for (int i = 1; i < GRAPH[vertex].length; i++) {
-			if(GRAPH[vertex][i] == 1 && !checked[i]){
-				checked[i] = true;
-                CNT++;
-				searchGraph(i);
+    static ArrayList<Integer>[] list = new ArrayList[1000 + 1];
+	private static void searchGraph_ArrayList(int vertex){
+		for (int i = 0; i < list[vertex].size(); i++) {
+			int currX = list[vertex].get(i);
+			if(!checked[currX]){
+				CNT++;
+				checked[currX] = true;
+				searchGraph_ArrayList(currX);
 			}
 		}
 	}
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
 		int n = Integer.parseInt(input[0]);
 		int m = Integer.parseInt(input[1]);
+		for (int i = 0; i <= n; i++) {
+			list[i] = new ArrayList<>();
+		}
 		for (int i = 0; i < m; i++) {
 			String[] pair = br.readLine().split(" ");
 			int start =Integer.parseInt(pair[0]);
 			int end =Integer.parseInt(pair[1]);
-			GRAPH[start][end] = 1;
-			GRAPH[end][start] = 1;
+			list[start].add(end);
+			list[end].add(start);
 		}
-        checked[1] = true;
-		searchGraph(1);
-
+		checked[1] = true;
+		searchGraph_ArrayList(1);
 		System.out.println(CNT);
     }
 }
