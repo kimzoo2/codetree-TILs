f2f99e0240ea86c8f6560e168e80d6825bf6d199
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-05-21 11:57:13 +0900

    Title: 수열 조작, Time: 212ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 240521/수열 조작/sequence-manipulation.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Deque<Integer> deque = new ArrayDeque<>();
+		for (int i = 1; i <= n; i++) {
+			deque.addLast(i);
+		}
+
+		while(deque.size() != 1){
+			deque.pollFirst();
+			int number = deque.pollFirst();
+			deque.addLast(number);
+		}
+		System.out.println(deque.peekLast());
+    }
+}
