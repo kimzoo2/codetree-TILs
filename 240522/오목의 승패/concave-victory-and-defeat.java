e00da01b15764b3f0e2a9b460afc18d1149ed718
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-05-22 12:25:15 +0900

    Title: 오목의 승패, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240522/오목의 승패/concave-victory-and-defeat.java
+++ 240522/오목의 승패/concave-victory-and-defeat.java
@@ -1,21 +1,20 @@
 import java.io.*;
 import java.util.Stack;
-
 public class Main {
 
     static int MAXLEN = 19;
-	static int[][] maps = new int[MAXLEN + 1][MAXLEN + 1];
-	static int[] dirX = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dirY = {0, 1, 1, 1, 0, -1, -1, -1};
-	static int ans = 0;
+	static int[][] maps = new int[MAXLEN][MAXLEN];
+	static int[] dirX = {-1, -1, 0, 1, 1};
+	static int[] dirY = {0, 1, 1, 1, 0};
+	static int ANS = 0;
 
-	private static boolean inRange(int x, int y, int xLen, int yLen){
+	private static boolean inRange(int x, int y, int xLen, int yLen) {
 		return x >= 0 && y >= 0 && x < xLen && y < yLen;
 	}
 
-    private static boolean isSameStone(int x, int y){
+	private static boolean isSameStone(int x, int y) {
 		Stack<Integer> stack = new Stack<>();
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < 5; i++) {
 			int nx = x;
 			int ny = y;
 			stack.clear();
@@ -24,40 +23,40 @@ public class Main {
 				nx += dirX[i];
 				ny += dirY[i];
 				int stone = stack.peek();
-				if(inRange(nx, ny, MAXLEN, MAXLEN) && stone == maps[nx][ny]){
+				if (inRange(nx, ny, MAXLEN, MAXLEN) && stone == maps[nx][ny]) {
 					stack.push(maps[nx][ny]);
 				}
 			}
-			if(stack.size() == 5) return true;
+			if (stack.size() == 5)
+				return true;
 		}
 		return false;
 	}
 
-    private static int[] findLocation(){
+    private static int[] findLocation() {
 		for (int i = 0; i < MAXLEN; i++) { // 가로
 			for (int j = 0; j < MAXLEN; j++) { // 세로
-				if(maps[j][i] != 0)
-					if(isSameStone(j, i)){
-						ans = maps[j][i];
-						return new int[] {j + 1, i + 1};
+				if (maps[i][j] != 0)
+					if (isSameStone(i, j)) {
+						ANS = maps[i][j];
+						return new int[] {i, j};
 					}
 			}
 		}
-		return new int[]{};
+		return new int[] {};
 	}
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for (int i = 0; i < MAXLEN; i++) {
 			String[] stones = br.readLine().split(" ");
 			for (int j = 0; j < MAXLEN; j++) {
 				maps[i][j] = Integer.parseInt(stones[j]);
 			}
 		}
-		int[] location = findLocation();
 
-		System.out.println(ans);
-		if(ans > 0)
-			System.out.println(location[0] + " " + location[1]);
+		int[] location = findLocation();
+		System.out.println(ANS);
+		if (ANS > 0)
+			System.out.println((location[0] + 1) + " " + (location[1] + 1));
     }
 }
