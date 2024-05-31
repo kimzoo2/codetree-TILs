72b5e76a3ee9a2e263d9a3827b388b2ffdc3047c
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-05-31 12:42:42 +0900

    Title: 기수 정렬 구현, Time: 908ms, Memory: 37MB, Status: Passed - CodeTree

--- 240531/기수 정렬 구현/implement-radix-sort.java
+++ 240531/기수 정렬 구현/implement-radix-sort.java
@@ -9,16 +9,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		String[] split = br.readLine().split(" ");
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(split[i]);
+		}
 
 		ArrayList[] arrayLists = new ArrayList[MAX_DIGIT];
-
+		int p = 1;
 		for (int i = MAX_K -1; i >= 0; i--) {
 			for (int j = 0; j < MAX_DIGIT; j++) {
-				arrayLists[j] = new ArrayList<String>();
+				arrayLists[j] = new ArrayList<Integer>();
 			}
-			for (String number : split) {
-				int digit = 0;
-				if(number.length() > i) digit = number.charAt(i)- '0';
+			for (int number : arr) {
+				int digit = (number / p) % 10;
 				// 자릿수 위치 버킷에 데이터 저장
 				arrayLists[digit].add(number);
 			}
@@ -26,13 +30,14 @@ public class Main {
 			for (int j = 0; j < MAX_DIGIT; j++) {
 				// 기존 배열에 자릿수 별로 정렬
 				for (int k = 0; k < arrayLists[j].size(); k++) {
-					split[idx++] = (String)arrayLists[j].get(k);
+					arr[idx++] = (int)arrayLists[j].get(k);
 				}
 			}
+			p *= 10;
 		}
 
-		for (String string : split) {
-			System.out.print(string + " ");
+		for (int i : arr) {
+			System.out.print(i + " ");
 		}
 	}
 }
