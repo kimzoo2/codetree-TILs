8fca3ff95afca53cc6282ec03e14eaf103c25166
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-06-11 16:04:38 +0900

    Title: 힙 정렬 구현, Time: 705ms, Memory: 28MB, Status: Passed - CodeTree

--- 240611/힙 정렬 구현/implement-heap-sort.java
+++ 240611/힙 정렬 구현/implement-heap-sort.java
@@ -9,50 +9,46 @@ public class Main {
 	}
 
 	public static void heapify(int[] arr, int n, int i){
-		int l = i * 2;
-		int r = i * 2 + 1;
 		int largest = i;
+		int l = 2 * i + 1;
+		int r = 2 * i + 2;
 
-		if(l < n && arr[l] > arr[largest])
+		if (l < n && arr[l] > arr[largest])
 			largest = l;
 
-
-		if(r < n && arr[r] > arr[largest])
+		if (r < n && arr[r] > arr[largest])
 			largest = r;
 
-		// 부모 노드가 변경된 경우
-		if(largest != i){
+		if (largest != i) {
 			swap(arr, i, largest);
 			heapify(arr, n, largest);
 		}
-
 	}
 
 	public static void heapSort(int[] arr, int n){
 		// max heap 만들기
-		for (int i = n/2; i > 0; i--) {
+		for (int i = n / 2 - 1; i >= 0; i--) {
 			heapify(arr, n, i);
 		}
 
 		// 정렬한다
-		for (int i = n-1; i > 0; i--) {
-			// swap
-			swap(arr, i, 1);
-			heapify(arr, i-1, 1);
+		for (int i = n - 1; i > 0; i--) {
+			swap(arr, 0, i);
+			heapify(arr, i, 0);
 		}
 	}
     public static void main(String[] args)  throws IOException {
-       BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		String[] split = br.readLine().split(" ");
-		int[] arr = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(split[i-1]);
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(split[i]);
 		}
 
-		heapSort(arr, n+1);
+		heapSort(arr, n);
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
 		}
     }
