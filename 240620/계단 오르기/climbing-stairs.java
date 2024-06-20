2227ea5b694678d2effc66839269ad7ae4d4e6a0
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-06-20 09:51:49 +0900

    Title: 계단 오르기, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240620/계단 오르기/climbing-stairs.java
+++ 240620/계단 오르기/climbing-stairs.java
@@ -4,14 +4,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[n + 1];
+		int[] dp = new int[1000 + 1];
+		dp[1] = 1;
 		dp[2] = 1;
-		if(n >= 3) {
-			dp[3] = 1;
-		}
+		dp[3] = 1;
 
-		for (int i = 4; i <= n; i++) {
-			dp[i] = (Math.max(dp[n-3], dp[n-2]) + 1) % 10_007;
+		for (int i = 5; i <= n; i++) {
+			if(i % 5 == 0){
+				dp[i] = dp[i - 5] + 1;
+			}
 		}
 
 		System.out.println(dp[n]);
