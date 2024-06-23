192f0f6c7ec85adb829e0eb88e6926b79e51ca34
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-06-23 18:53:35 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 315ms, Memory: 14MB, Status: Passed - CodeTree

--- 240623/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 240623/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -7,7 +7,6 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 		int[][] arr = new int[n][2];
 		int[] memo = new int[n];
-		int[] line = new int[1000 + 1];
 
 		Arrays.fill(memo, 1);
 
@@ -19,33 +18,22 @@ public class Main {
 
 		Arrays.sort(arr, Comparator.comparingInt(a -> a[1]));
 
-		int max = 1;
 		for (int i = 0; i < n; i++) {
-			int cnt = 1;
-			fillLine(arr[i][0], arr[i][1], line);
-			for (int j = 0; j < n; j++) {
-				if(i == j) continue;
-				if(fillLine(arr[j][0], arr[j][1], line)){
-					cnt++;
+
+			for (int j = 0; j < i; j++) {
+				int x1I = arr[i][0];
+				int x2J = arr[j][1];
+
+				if(x2J < x1I){
+					memo[i] = Math.max(memo[i], memo[j] + 1);
 				}
 			}
-			max = Math.max(max, cnt);
-			// 선분 배열 초기화
-			Arrays.fill(line, 0);
 		}
 
-		System.out.println(max);
+		int ans = 0;
+		for(int i = 0; i < n; i++)
+			ans = Math.max(ans, memo[i]);
+
+		System.out.print(ans);
     }
-
-    private static boolean fillLine(int x, int y, int[] line){
-		// 겹친 선분이 있으면 채우지 않는다.
-		for (int i = x-1; i <= y-1; i++) {
-			if(line[i] == 1) return false;
-		}
-
-		for (int i = x-1; i <= y-1; i++) {
-			line[i] = 1;
-		}
-		return true;
-	}
 }
