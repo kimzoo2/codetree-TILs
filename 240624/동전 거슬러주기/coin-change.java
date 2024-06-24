3b9ea5ecd86acb8ae471a91551029f36525b28fc
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-06-24 10:30:07 +0900

    Title: 동전 거슬러주기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240624/동전 거슬러주기/coin-change.java
+++ 240624/동전 거슬러주기/coin-change.java
@@ -15,14 +15,15 @@ public class Main {
 		}
 
 		int[] memo = new int[m + 1];
-		// initialize
-		for (int i = 1; i < m + 1; i++) {
-			memo[i] = Integer.MAX_VALUE;
-		}
 
+		// 초기화
+		for (int i = 1; i <= m; i++) {
+			memo[i] = 10_000;
+		}
 		for (int i = 0; i < n; i++) { // 동전 타입
-			for (int j = 0; j <= m; j++) { // 동전 금액
-				if(j - arr[i] >= 0){
+			for (int j = arr[i]; j <= m; j++) { // 동전 금액
+				
+				if (j - arr[i] >= 0) {
 					memo[j] = Math.min(memo[j], memo[j - arr[i]] + 1);
 				}
 			}
