cfe17a0c1ed05e473ca953d5bb25b4eac774c3b8
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-06-24 09:48:27 +0900

    Title: 동전 거슬러주기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240624/동전 거슬러주기/coin-change.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputs = br.readLine().split(" ");
+		int n = Integer.parseInt(inputs[0]);
+		int m = Integer.parseInt(inputs[1]);
+		int[] arr = new int[n];
+		String[] tokenType = br.readLine().split(" ");
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(tokenType[i]);
+		}
+
+		int[] memo = new int[m + 1];
+		// initialize
+		for (int i = 1; i < m + 1; i++) {
+			memo[i] = Integer.MAX_VALUE;
+		}
+
+		for (int i = 0; i < n; i++) { // 동전 타입
+			for (int j = 0; j <= m; j++) { // 동전 금액
+				if(j - arr[i] >= 0){
+					memo[j] = Math.min(memo[j], memo[j - arr[i]] + 1);
+				}
+			}
+		}
+
+		System.out.println(memo[m]);
+    }
+}
