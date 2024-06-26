9b5a4e40c303e6ff344423eeb8af13fdc08640d2
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-06-26 17:09:55 +0900

    Title: 계단 오르기 2, Time: 124ms, Memory: 9MB, Status: Passed - CodeTree

--- 240626/계단 오르기 2/climbing-stairs-2.java
+++ 240626/계단 오르기 2/climbing-stairs-2.java
@@ -1,32 +1,37 @@
 import java.io.*;
 public class Main {
+    
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int[][] dp = new int[4][n + 2];
+		int[] arr = new int[n + 1];
+		int[][] dp = new int[4][n + 1];
 
 		String[] inputs = br.readLine().split(" ");
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(inputs[i]);
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(inputs[i - 1]);
 		}
 
-		dp[1][n] = arr[n - 1];
-		for (int i = n - 1; i >= 1; i--) {
-			for (int j = 1; j <= 3; j++) {
-				if(dp[j - 1][i + 1] > 0 || dp[j][i + 2] > 0) {
-					dp[j][i] = Math.max(dp[j - 1][i + 1], dp[j][i + 2]) + arr[i - 1];
+		dp[1][1] = arr[1];
+		dp[0][2] = arr[2];
+		dp[2][2] = arr[1] + arr[2];
+		for (int i = 3; i <= n; i++) {
+			for (int j = 0; j <= 3; j++) {
+				if(j == 0 && dp[j][i - 2] > 0){
+					dp[j][i] = dp[j][i - 2] + arr[i];
+					continue;
+				}
+				if(j > 0 && dp[j - 1][i - 1] > 0) {
+					dp[j][i] = Math.max(dp[j - 1][i - 1], dp[j][i - 2]) + arr[i];
 				}
 			}
 		}
-		
+
 		int max = 0;
-		for (int i = 1; i <= 3; i++) {
-			for (int j = 1; j <= 2; j++) {
-				max = Math.max(dp[i][j], max);
-			}
+		for (int i = 0; i <= 3; i++) {
+			max = Math.max(dp[i][n], max);
 		}
 
 		System.out.println(max);
-    }
+	}
 }
