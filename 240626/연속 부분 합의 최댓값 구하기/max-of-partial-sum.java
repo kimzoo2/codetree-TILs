2c2bfbc9281b7f2ff74fc668763be3e158aab05e
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-06-26 09:33:12 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 318ms, Memory: 25MB, Status: Passed - CodeTree

--- 240626/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
+++ 240626/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -4,24 +4,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
-		int[] memo = new int[n + 1];
+		int[] memo = new int[n];
 
 		String[] inputs = br.readLine().split(" ");
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(inputs[i]);
 		}
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i < n; i++) {
 			memo[i] = Integer.MIN_VALUE;
 		}
 		memo[0] = arr[0];
-		for (int i = 1; i <= n; i++) {
-			memo[i] = Math.max(arr[i -1], memo[i - 1] + arr[i - 1]);
+		for (int i = 1; i < n; i++) {
+			memo[i] = Math.max(arr[i], memo[i - 1] + arr[i]);
 		}
 
 		int max = Integer.MIN_VALUE;
 
-		for (int i = 0; i <= n; i++) {
+		for (int i = 0; i < n; i++) {
 			max = Math.max(max, memo[i]);
 		}
 
