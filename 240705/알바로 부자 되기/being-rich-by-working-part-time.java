6b63b2d9ed4ccf3cbaf1d759db982b3df075e529
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-07-05 11:52:39 +0900

    Title: 알바로 부자 되기, Time: 237ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240705/알바로 부자 되기/being-rich-by-working-part-time.java
+++ 240705/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -14,31 +14,28 @@ public class Main {
 		}
 
 		Arrays.sort(arr, ((o1, o2) -> o1[1] - o2[1]));
-
-		int[] dp = new int[arr[n-1][1] + 1];
-		dp[arr[0][1]] = arr[0][2];
+		int[][] dp = new int[n][2];
+		dp[0][0] = arr[0][1];
+		dp[0][1] = arr[0][2];
 
 		for (int i = 1; i < n; i++) {
-			int start = arr[i][0];
-			int end = arr[i][1];
-			int value = arr[i][2];
-			// 해당 알바와 기간이 겹치지만 더 큰 만족도를 주는 알바 여부
-			for (int j = end; j >= start; j--) {
-				dp[end] = Math.max(dp[end], Math.max(dp[j], value));
-			}
-			// 이전에 했던 알바와 누적 합산한다.
-			for (int j = start-1; j >= 0 ; j--) {
-				// 이전 알바가 있으나 기간이 겹쳐 0까지 가는 경우
-				if(j == 0){
-					dp[end] = Math.max(dp[end], value);
+			dp[i][0] = arr[i][1];
+			for (int j = i -1; j >= 0; j--) {
+				// 기간이 겹치면서 큰 만족도를 주는 알바 여부
+				if(arr[i][0] <= dp[j][0]) {
+					dp[i][1] = Math.max(arr[i][2], dp[j][1]);
 				}
-				if(dp[j] > 0){
-					dp[end] = Math.max(dp[end], dp[j] + value);
+
+				// 기간이 겹치지 않을 때
+				if(arr[i][0] > dp[j][0]){
+					// 기간이 겹치지 않을 때와 겹칠 때를 비교하여 pay가 더 큰 경우를 누적한다.
+					dp[i][1] = Math.max(dp[i][1], arr[i][2] + dp[j][1]);
 					break;
 				}
 			}
+
 		}
 
-		System.out.println(dp[arr[n-1][1]]);
+		System.out.println(dp[n-1][1]);
     }
 }
