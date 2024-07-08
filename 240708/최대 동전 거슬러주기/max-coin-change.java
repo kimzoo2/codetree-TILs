d921bb3c5aaad4291fcaa1a5e16e9416b49db9dc
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-07-08 14:54:55 +0900

    Title: 최대 동전 거슬러주기, Time: 128ms, Memory: 9MB, Status: Passed - CodeTree

--- 240708/최대 동전 거슬러주기/max-coin-change.java
+++ 240708/최대 동전 거슬러주기/max-coin-change.java
@@ -16,11 +16,9 @@ public class Main {
 
 		for (int i = 0; i < n; i++) {
 			tokens[i] = Integer.parseInt(tokenInputs[i]);
-            if(tokens[i] > m){
-				System.out.println(-1);
-				return;
+           if(tokens[i] <= m){
+				dp[tokens[i]] = 1;
 			}
-			dp[tokens[i]] = 1;
 		}
 
 		for (int i = 0; i < n; i++) { // token 반복
