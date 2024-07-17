7c6fb1721cd000c86300606ccba1c77fca554073
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-07-17 16:59:43 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 1132ms, Memory: 23MB, Status: Passed - CodeTree

--- 240717/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
+++ 240717/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -1,40 +1,42 @@
-import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
+    public static int k, n;
+    public static ArrayList<Integer> selectedNums = new ArrayList<>();
+    
+    // 선택된 원소들을 출력해줍니다.
+    public static void printPermutation() {
+        for(int i = 0; i < selectedNums.size(); i++)
+            System.out.print(selectedNums.get(i) + " ");
+        System.out.println();
+    }
 
-    private static int n,k;
+    public static void findPermutations(int cnt) {
+        // n개를 모두 뽑은 경우 답을 출력해줍니다.
+        if(cnt == n) {
+            printPermutation();
+            return;
+        }
 
-	private static boolean isDuplicated(int cnt, int n, int[] arr){
-		if(cnt < 1) return false;
-		for (int i = cnt; i >= 0 && i > cnt - 2; i--) {
-			if(arr[i] != n) return false;
-		}
-		return true;
-	}
-	private static void bfs(int cnt, int[] arr) {
-		if(k == cnt){
-			for (int i : arr) {
-				System.out.print(i + " ");
-			}
-			System.out.println();
-			return;
-		}
+        for(int i = 1; i <= k; i++) {
+            if(cnt >= 2 && 
+               i == selectedNums.get(selectedNums.size() - 1) && 
+               i == selectedNums.get(selectedNums.size() - 2))
+               continue;
 
-		for (int i = 1; i <= n; i++) {
-			if(isDuplicated(cnt-1, i, arr)) continue;
-			arr[cnt] = i;
-			bfs(cnt+1, arr);
-			arr[cnt] = 0;
-		}
-	}
+            selectedNums.add(i);
+            findPermutations(cnt + 1);
+            selectedNums.remove(selectedNums.size() - 1);
+        }
+    }
 
-    public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] inputs = br.readLine().split(" ");
-		n = Integer.parseInt(inputs[0]); // 숫자
-		k = Integer.parseInt(inputs[1]); // 자릿수
-		int[] arr = new int[k];
-		bfs(0, arr);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        k = sc.nextInt();
+        n = sc.nextInt();
+        
+		findPermutations(0);
 	}
 }
