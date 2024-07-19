97f57b4ee68fabd65b7f0f5c2aae342cc82675b2
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-07-19 11:27:29 +0900

    Title: n개 중에 m개 뽑기, Time: 148ms, Memory: 9MB, Status: Passed - CodeTree

--- 240719/n개 중에 m개 뽑기/n-choose-m.java
+++ 240719/n개 중에 m개 뽑기/n-choose-m.java
@@ -17,24 +17,15 @@ public class Main {
 		return false;
 	}
 
-	private static void bfs3(int cnt, int[] arr){
+	private static void bfs(int cnt, int num, int[] arr){
 		if(cnt == m){
 			print(arr);
 			return;
 		}
 
-		for (int i = 1; i <= N; i++) {
-			if(!isDuplicated(i, arr)) {
-				if(cnt > 0){
-					if(arr[cnt - 1] < i){
-						arr[cnt] = i;
-						bfs3(cnt + 1, arr);
-					}
-				}else{
-					arr[cnt] = i;
-					bfs3(cnt + 1, arr);
-				}
-			}
+		for (int i = num; i <= N; i++) {
+			arr[cnt] = i;
+			bfs(cnt + 1, i + 1, arr);
 			arr[cnt] = 0;
 		}
 	}
@@ -45,6 +36,6 @@ public class Main {
 		N = Integer.parseInt(inputs[0]);
 		m = Integer.parseInt(inputs[1]);
 		int[] arr = new int[m];
-		bfs3(0, arr);
+		bfs(0, 1, arr);
 	}
 }
