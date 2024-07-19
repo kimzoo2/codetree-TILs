7eaf7caa1057dd2c0a8b24702cc9952d51be9903
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-07-19 14:14:48 +0900

    Title: xor 결과 최대 만들기, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240719/xor 결과 최대 만들기/max-of-xor.java
+++ 240719/xor 결과 최대 만들기/max-of-xor.java
@@ -3,9 +3,10 @@ public class Main {
     private static int n,m,max;
 	private static int[] combinationArr = new int[20];
 
-	private static void choose(int[] nums, int cnt, int lastNum) {
+	private static void choose(int[] nums, int cnt, int idx) {
 		// cnt가 m일 때 재귀함수를 종료한다.
 			// combinationArr에 들어있는 값들의 xor 값과 max 중 큰 값을 넣는다.
+		if(idx >= n) return;
 		if(cnt == m){
 			int ans = 0;
 			for (int i = 0; i < m; i++) {
@@ -15,13 +16,11 @@ public class Main {
 			return;
 		}
 
-		// 반복문을 돌아가며 combinationArr에 xor할 값을 넣는다.
-		for (int i = cnt; i < n; i++) {
-			if(lastNum < nums[i]) {
-				combinationArr[cnt] = nums[i];
-				choose(nums, cnt + 1, i);
-			}
-		}
+		combinationArr[cnt] = nums[idx];
+		choose(nums, cnt + 1, idx + 1);
+		combinationArr[cnt] = 0;
+
+		choose(nums, cnt, idx + 1);
 	}
     public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
