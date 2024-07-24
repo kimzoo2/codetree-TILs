42e9a97888da7dfcd156716a0e702ba3f4684c71
Juhee Kim <81366324+kimzoo2@users.noreply.github.com>
2024-07-24 12:12:49 +0900

    Title: 크기가 n인 순열, Time: 1061ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/크기가 n인 순열/n-permutation.java
+++ 240724/크기가 n인 순열/n-permutation.java
@@ -3,31 +3,31 @@ import java.util.*;
 
 public class Main {
     private static int n;
-    private static List<Integer> list = new ArrayList<>();
 
-	private static void printList(){
-		for (Integer i : list) {
-			System.out.print(i + " ");
+	private static void printList(int[] arr){
+		for (int i = 0; i <n; i++) {
+			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 	}
-	private static void choose(int cnt, boolean[] visited) {
+	private static void choose(int cnt, boolean[] visited, int[] ansArr) {
 		if(n == cnt){
-			printList();
+			printList(ansArr);
 			return;
 		}
 		for (int i = 1; i <= n; i++) {
-			if(visited[i]) continue;
-            visited[i] = true;
-            list.add(cnt, i);
-            choose(cnt + 1, visited);
-            visited[i] = false;
-            list.remove(cnt);
+			if(!visited[i]) {
+				visited[i] = true;
+				ansArr[cnt] = i;
+				choose(cnt + 1, visited, ansArr);
+				visited[i] = false;
+			}
 		}
 	}
     public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		choose(0, new boolean[n+1]);
+		int[] ansArr = new int[n];
+		choose(0, new boolean[n + 1], ansArr);
     }
 }
